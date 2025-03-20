1f7dcefabe23e2f1b82f954a324c0a35c68131de
bashmachenkov313 <bashmachenkovoleg@yandex.ru>
2025-03-20 15:55:20 +0300

    набросок экрана настроек

--- lib/features/SettingsScreen/view/Settings.dart
+++ lib/features/SettingsScreen/view/Settings.dart
@@ -1,17 +1,17 @@
-import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
-class SettingsScreen extends  StatefulWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
-
 
   @override
   State<SettingsScreen> createState() => _SettingsScreenState();
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
-
+  bool _isHistorySavingEnabled = true;
+  bool themeProvider = true;
 
 
   @override
@@ -21,17 +21,59 @@ class _SettingsScreenState extends State<SettingsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        appBar: AppBar(
-          title: const Text("Настройки"),
-          centerTitle: true,
+    final theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Настройки"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Theme Switch
+            ListTile(
+              leading: Icon(
+                themeProvider == true
+                    ? Icons.dark_mode
+                    : Icons.light_mode,
+              ),
+              title: const Text("Включить тёмную тему"),
+              trailing: Switch(
+                value: themeProvider,
+                onChanged: (value) {
+                  setState(() {
+                    themeProvider = value;
+                  });
+                },
+              ),
+            ),
+            // History Saving Switch
+            ListTile(
+              leading: const Icon(Icons.history),
+              title: const Text("Сохранять историю"),
+              trailing: Switch(
+                value: _isHistorySavingEnabled,
+                onChanged: (value) {
+                  setState(() {
+                    _isHistorySavingEnabled = value;
+                  });
+                },
+              ),
+            ),
+            const Spacer(),
+            // App Version
+            Center(
+              child: Text(
+                "Версия приложения: 1.0.0",
+                style: TextStyle(color: theme.hintColor),
+              ),
+            ),
+            const SizedBox(height: 10),
+          ],
         ),
-        body: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Center(
-                child: Text("Настройки")
-            )
-        )
+      ),
     );
   }
 }
