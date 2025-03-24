2c904b204b572257b89ffb6fa4b78b98337ed52b
bashmachenkov313 <bashmachenkovoleg@yandex.ru>
2025-03-24 17:46:41 +0300

    Добавил equatable

--- lib/blocks/theme_block/theme_cubit.dart
+++ lib/blocks/theme_block/theme_cubit.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:equatable/equatable.dart';
+import "package:equatable/equatable.dart";
 
 part 'theme_state.dart';
 
