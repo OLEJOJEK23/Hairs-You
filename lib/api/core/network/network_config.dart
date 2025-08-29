4bb65b973149575b15183492b142d66ecb716d42
Олег <113263638+OLEJOJEK23@users.noreply.github.com>
2025-08-30 02:28:11 +0300

    перенёс api key

--- lib/api/core/network/network_config.dart
+++ lib/api/core/network/network_config.dart
@@ -2,5 +2,5 @@ class NetworkConfig {
   static const String baseUrl = 'https://olejojek.ru/api';
   static const String googleMapsBaseUrl =
       'https://maps.googleapis.com/maps/api/place';
-  static const String apiKey = 'AIzaSyBgy6Dza_gIvk2IcaeItlOU9ZBwl1CykL4';
+  static const String apiKey = 'api_key';
 }
